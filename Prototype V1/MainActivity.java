2425bd330278624ec2af404b4096c29a434966dc
Luis Cruz <49624124+NightPanda70@users.noreply.github.com>
2022-04-26 16:43:24 -0700

    Update MainActivity.java

--- Prototype V1/MainActivity.java
+++ Prototype V1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bluetooth_controller;
 
 import android.Manifest;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -11,49 +12,84 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import java.util.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 0;
+    private BluetoothAdapter bluetoothAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+    //private ArrayList BTList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = findViewById(R.id.btn1);
+        // the buttons and other stuff for the GUI
+        Button btnF = findViewById(R.id.btn1);
+        Button btnB = findViewById(R.id.btn2);
+        Button btnL = findViewById(R.id.btn3);
+        Button btnR = findViewById(R.id.btn4);
         TextView txt1 = findViewById(R.id.txt);
 
-        TurnOnBt();
 
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt1.setText("change");
-            }
-        });
-    }
-
-    private void TurnOnBt(){
+        // setting up bluetooth
         BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothAdapter = bluetoothManager.getAdapter();
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
             Toast.makeText(this, "Bluetooth is not supported", Toast.LENGTH_SHORT).show();
         }
 
+        // turn BT on
+        TurnOnBt();
+        // list
+        listDevices();
+
+        // Button Listeners
+        btnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Forward");
+            }
+        });
+
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Back");
+            }
+        });
+
+        btnL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Left");
+            }
+        });
+
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Right");
+            }
+        });
+    }
+
+    private void TurnOnBt(){
         // if bluetooth is not enabled ask to turn it on
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             Toast.makeText(this, "Bluetooth is currently OFF", Toast.LENGTH_SHORT).show();
         }
-
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.BLUETOOTH_CONNECT}, 1);
+        }
     } // end of turn on BT
 
     @Override
@@ -64,4 +100,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_SHORT).show();
         }
     }
-}
+    // give a list of connected devices
+    private void listDevices(){
+        // has to do with getting an error
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.BLUETOOTH_CONNECT}, 1);
+        }
+
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    }
+
+
+} // end of class
